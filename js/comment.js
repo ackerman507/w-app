4f7c024707cf271a0b5d10e518a56b7b8d3c3d5c
ackerman507 <ackerman507@hotmail.com>
2024-06-19 22:15:29 -0600

    change heart color

--- js/comment.js
+++ js/comment.js
@@ -187,7 +187,7 @@ export const comment = (() => {
 
         changeButton(id, true);
         const tmp = button.innerText;
-        button.innerText = 'Publicando..';
+        button.innerText = 'Cargando..';
 
         const status = await request(HTTP_GET, '/api/comment/' + id)
             .token(session.get('token'))
--- js/util.js
+++ js/util.js
@@ -124,7 +124,8 @@ export const util = (() => {
     const animation = () => {
         const duration = 15 * 1000;
         const animationEnd = Date.now() + duration;
-        const colors = ["#FFC0CB", "#FF1493", "#C71585"];
+        // const colors = ["#FFC0CB", "#FF1493", "#C71585"];
+        const colors = ["#FFF"];
 
         const randomInRange = (min, max) => {
             return Math.random() * (max - min) + min;
